1f95709fd94ea6ab773abafc69ebb359f966e2c5
LailaVivianne <lailavivianne@alumnos.cei.es>
2023-03-25 01:11:38 +0100

    new done

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,14 @@ console.log('App id loading');
 
 document.getElementById("mobile-menu").addEventListener("click", function (){
     document.getElementById("desplegable").classList.toggle("d-none");
-})
+});
+
+document.getElementById("delicious").addEventListener("click", function (){
+    document.querySelector(".delicious").classList.remove("d-none");
+    document.querySelector(".coffee").classList.add("d-none")
+});
+
+document.getElementById("coffee").addEventListener("click", function(){
+    document.querySelector(".coffee").classList.remove("d-none")
+    document.querySelector(".delicious").classList.add("d-none")
+});
